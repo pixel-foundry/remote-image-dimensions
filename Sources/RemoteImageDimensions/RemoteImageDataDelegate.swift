b9aacdcc65ac4ac89be8e45cfacbedcf114be650
Hal Lee <hal@lee.me>
2020-11-01 10:41:55 -0500

    Perform delegate tasks on a single utility queue

--- Sources/RemoteImageDimensions/RemoteImageDataDelegate.swift
+++ Sources/RemoteImageDimensions/RemoteImageDataDelegate.swift
@@ -5,30 +5,38 @@ import FoundationNetworking
 
 final class RemoteImageDataDelegate: NSObject, URLSessionDataDelegate {
 
+	private let queue = DispatchQueue(label: "RemoteImageDataDelegateQueue", qos: .utility)
+
 	var taskDelegates = [ImageDimensionTaskDelegate]()
 
 	func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-		guard let delegate = taskDelegates.first(where: { $0.task === dataTask }) else { return }
-		let completed = delegate.process(data: data)
-		if completed {
-			cleanup(for: dataTask)
+		queue.async {
+			guard let delegate = self.taskDelegates.first(where: { $0.task === dataTask }) else { return }
+			let completed = delegate.process(data: data)
+			if completed {
+				self.cleanup(for: dataTask)
+			}
 		}
 	}
 
 	func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Swift.Error?) {
-		guard let delegate = taskDelegates.first(where: { $0.task === task }) else { return }
-		delegate.complete(with: error)
-		cleanup(for: task)
+		queue.async {
+			guard let delegate = self.taskDelegates.first(where: { $0.task === task }) else { return }
+			delegate.complete(with: error)
+			self.cleanup(for: task)
+		}
 	}
 
 	func cleanup(for task: URLSessionTask) {
-		taskDelegates.removeAll(where: {
-			if $0.task === task || $0.task == nil {
-				$0.task?.cancel()
-				return true
-			}
-			return false
-		})
+		queue.async {
+			self.taskDelegates.removeAll(where: {
+				if $0.task === task || $0.task == nil {
+					$0.task?.cancel()
+					return true
+				}
+				return false
+			})
+		}
 	}
 
 }
